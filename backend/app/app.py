4c01bd17ef1d19e844a032040ae04b07d42ef843
jara22222 <jarajoaquin750@gmail.com>
2026-01-12 10:45:08 +0800

    Update CORS for production

--- backend/app/app.py
+++ backend/app/app.py
@@ -25,8 +25,9 @@ async def lifespan(app:FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+   "https://pixelshare-frontend.vercel.app",
+   "http://localhost:5173",
+   "http://127.0.0.1:5173",
 ]
 
 app.add_middleware(
